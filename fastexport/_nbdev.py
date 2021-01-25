54b0330a99b39f1fdfb28fb3bc29ac11cb73aae4
Thomas Capelle <thomas.capelle@steady-sun.com>
2021-01-25 18:38:30 +0100

    reverted head'

--- fastexport/_nbdev.py
+++ fastexport/_nbdev.py
@@ -5,8 +5,7 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"TensorBase.requires_grad_": "00_jit.ipynb",
          "Learner.to_jit": "00_jit.ipynb"}
 
-modules = ["jit.py",
-           "export.py"]
+modules = ["jit.py"]
 
 doc_url = "https://muellerzr.github.io/fastexport/"
 
